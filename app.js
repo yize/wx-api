ec497af10f261a54f9bad230b8fb822c184794c2
iThan <hanchao.shc@alibaba-inc.com>
2015-05-15 15:56:43 +0800

    update

---
+++ app.js
@@ -0,0 +1,44 @@
+var koa = require('koa');
+var app = koa();
+
+var crypto = require('crypto');
+
+app.use(function *(){
+
+	var TOKEN = 'yizecoala'
+	var sha1 = crypto.createHash('sha1');
+
+	var signature = this.request.query.signature;
+	var timestamp = this.request.query.timestamp;
+	var nonce = this.request.query.nonce;
+
+	if(checkSignature(signature,timestamp,nonce)){
+		this.body = true;
+	}else{
+		this.body = false;
+	}
+});
+
+function checkSignature(signature,timestamp,nonce){
+	var TOKEN = 'yizecoala'
+
+	var tmpArr = [TOKEN , timestamp , nonce];
+	tmpArr.sort();
+
+	var tmpStr = tmpArr.join('');
+
+	var sha1 = crypto.createHash('sha1');
+
+	sha1.update(tmpStr)
+	var hex = sha1.digest('hex');
+
+	if(hex === signature){
+		console.log('验证成功');
+		return true;
+	}else{
+		console.log('验证失败');
+		return false;
+	}
+}
+
+app.listen(1111);
